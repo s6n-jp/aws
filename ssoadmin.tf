6c1610b017b9ea746b6e6f480615ce2e15f219a7
Naoki Ikeguchi <me@s6n.jp>
2024-10-23 23:53:04 +0900

    feat: Assign isucon14 account to siketyan

--- ssoadmin.tf
+++ ssoadmin.tf
@@ -19,3 +19,12 @@ resource "aws_ssoadmin_account_assignment" "siketyan_admin" {
   target_id          = aws_organizations_account.default.id
   target_type        = "AWS_ACCOUNT"
 }
+
+resource "aws_ssoadmin_account_assignment" "isucon14_admin" {
+  instance_arn       = data.aws_ssoadmin_instances.default.arns[0]
+  permission_set_arn = aws_ssoadmin_permission_set.admin.arn
+  principal_id       = aws_identitystore_user.siketyan.user_id
+  principal_type     = "USER"
+  target_id          = aws_organizations_account.isucon14.id
+  target_type        = "AWS_ACCOUNT"
+}
