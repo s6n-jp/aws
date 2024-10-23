0675704edcb9eb6cc595ea7f4b9b93fafd95a031
Naoki Ikeguchi <me@s6n.jp>
2024-10-23 23:18:26 +0900

    feat: Add an AWS account for ISUCON14

--- organizations.tf
+++ organizations.tf
@@ -3,3 +3,13 @@ resource "aws_organizations_organization" "default" {
   aws_service_access_principals = ["sso.amazonaws.com"]
   enabled_policy_types          = ["AISERVICES_OPT_OUT_POLICY"]
 }
+
+resource "aws_organizations_account" "default" {
+  name  = "s6n.jp"
+  email = "me@s6n.jp"
+}
+
+resource "aws_organizations_account" "isucon14" {
+  name  = "isucon14"
+  email = "aws+isucon14@s6n.jp"
+}
